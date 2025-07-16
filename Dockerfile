eafec0e3dd42815cd4590144eefa6f22fbd4fd49
Your Name <yanw1693@gmail.com>
2025-07-16 21:46:56 +0200

    test with desktop-linux

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-# Use the latest Windows Server Core image
-FROM mcr.microsoft.com/windows/servercore:ltsc2025
-
-RUN echo "hello"
--- Test.ps1
+++
@@ -1,2 +0,0 @@
-winget install --id Docker.DockerDesktop --silent --accept-package-agreements --accept-source-agreements
-winget install --id GoLang.Go --silent --accept-package-agreements --accept-source-agreements
